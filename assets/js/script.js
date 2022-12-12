addd0c7bcebd828cecf861692e47551cca598b18
mqas1 <114903874+mqas1@users.noreply.github.com>
2022-12-12 22:43:30 +1100

    adds date to header using jsday

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,4 +20,15 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+
+    var currentDayEl = $('#currentDay');
+    dayjs.extend(window.dayjs_plugin_advancedFormat);
+    function displayCurrentDay () {
+        
+        var currentDay = dayjs().format('dddd, MMMM Do');
+        currentDayEl.text(currentDay);
+    }
+
+    displayCurrentDay();
+
   });
